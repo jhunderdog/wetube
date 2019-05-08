2d51fd1375eeefd28c0498979fe0b3231b1f23ec
jh <33382water@gmail.com>
2019-05-08 22:55:21 +0900

    Handling Routes with Express

--- index.js
+++ index.js
@@ -5,5 +5,15 @@ const PORT = 4000;
 function handleListening(){
     console.log(`Listening on: http://localhost:${PORT}`);
 }
+function handleHome(req, res){
+   console.log(req);
+   res.send("Hello from Home");
+}
 
+function handleProfile(res, res){
+    res.send("You are on my profile");
+}
+
+app.get("/", handleHome)
+app.get("/profile",handleProfile)
 app.listen(PORT, handleListening);
