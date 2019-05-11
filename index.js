cbb1693d2d7dbdda5096406cd9d53fcf1f7192ef
jh <33382water@gmail.com>
2019-05-11 19:46:16 +0900

    Express Core:Middlewares part Two

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -15,6 +20,12 @@ const betweenHome = (req, res, next) => {
     console.log("Between")
     next();
 }
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.use(betweenHome);
 app.get("/", handleHome)
