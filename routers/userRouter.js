364542b31e546aa9bce7de7c87c849f7a02636a8
jh <33382water@gmail.com>
2019-05-14 20:46:42 +0900

    Change Profile HTML

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
+import { editProfile, changePassword } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get(routes.users, (req, res) => res.send('users'))
+userRouter.get(routes.editProfile, editProfile)
+userRouter.get(routes.changePassword, changePassword)
 userRouter.get(routes.userDetail, (req, res) => res.send('userDetail'))
-userRouter.get(routes.editProfile, (req, res) => res.send('editProfile'))
-userRouter.get(routes.changePassword, (req, res) => res.send('changePassword'))
 
 export default userRouter;
