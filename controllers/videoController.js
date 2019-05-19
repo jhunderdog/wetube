ef0f7decac8ece81cada946dd2fe5b9215ae6787
jh <33382water@gmail.com>
2019-05-19 22:46:17 +0900

    Join Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -6,7 +6,7 @@ res.render("home", { pageTitle: 'Home', videos });
 export const search = (req, res) => {
     const { query: {term : searchingBy } } = req;
     // const searchingBy = req.query.term;
-    res.render("search", { pageTitle: "Search", searchingBy });
+    res.render("search", { pageTitle: "Search", searchingBy, videos });
 }
 
 export const upload = (req, res) => res.render("upload", { pageTitle: 'Upload' });
