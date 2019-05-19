804fdfb95b8a1f441268a6cc3003808e0decab1f
jh <33382water@gmail.com>
2019-05-19 13:32:14 +0900

    Home Controller part Two

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
         title:"Video awesome",
         description: "This is something I love",
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name:"Nicolas",
@@ -17,7 +17,7 @@ export const videos = [
         title:"Video super",
         description: "This is something I love",
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name:"Nicolas",
@@ -30,7 +30,7 @@ export const videos = [
         title:"Video nice",
         description: "This is something I love",
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name:"Nicolas",
@@ -43,7 +43,7 @@ export const videos = [
         title:"Video perfect",
         description: "This is something I love",
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name:"Nicolas",
