616bfa7005d39ebb5780d6f3334abf6fac38fb36
jh <33382water@gmail.com>
2019-05-19 13:07:34 +0900

    Home Controller

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,10 +3,10 @@ import routes from "../routes";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, (req, res) => res.redner('Videos'))
-videoRouter.get(routes.upload, (req, res) => res.redner('Upload'))
-videoRouter.get(routes.videoDetail, (req, res) => res.redner('Video Detail'))
-videoRouter.get(routes.editVideo, (req, res) => res.redner('Edit Video'))
-videoRouter.get(routes.deleteVideo, (req, res) => res.redner('Delete Video'))
+videoRouter.get(routes.videos, (req, res) => res.render('Videos'))
+videoRouter.get(routes.upload, (req, res) => res.render('Upload'))
+videoRouter.get(routes.videoDetail, (req, res) => res.render('Video Detail'))
+videoRouter.get(routes.editVideo, (req, res) => res.render('Edit Video'))
+videoRouter.get(routes.deleteVideo, (req, res) => res.render('Delete Video'))
 
 export default videoRouter;
