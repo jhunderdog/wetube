e8acc6b945e50e4051b750df1eb7d595c9875c7a
jh <33382water@gmail.com>
2019-05-20 22:31:44 +0900

    Log In and User Profile Controller

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
 import routes from "../routes";
-import { editProfile, changePassword } from "../controllers/userController";
+import { userDetail, editProfile, changePassword } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get(routes.users, (req, res) => res.send('users'))
 userRouter.get(routes.editProfile, editProfile)
 userRouter.get(routes.changePassword, changePassword)
-userRouter.get(routes.userDetail, (req, res) => res.send('userDetail'))
+userRouter.get(routes.userDetail(), userDetail)
 
 export default userRouter;
