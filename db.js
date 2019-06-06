fb4e1c69ca459dc4d78c5ebb5d372a364d39d784
jh <33382water@gmail.com>
2019-06-06 18:19:44 +0900

    Configuring Dot Env

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect("mongodb://localhost:27017/we-tube", 
+mongoose.connect(
+    process.env.MONGO_URL,
     {
         useNewUrlParser: true,
         useFindAndModify: false
@@ -10,5 +13,6 @@ mongoose.connect("mongodb://localhost:27017/we-tube",
 const db = mongoose.connection;
 const handleOpen = () => console.log("Connected to DB")
 const handleError = (error) => console.log(`Error on DB connection:${error}`)
+
 db.once("open", handleOpen);
 db.on("error", handleError);
